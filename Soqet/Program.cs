7ac75064a7a426a42e571ad1637520d81014c405
Alessandro Proto <me@alexdevs.me>
2022-02-01 09:57:05 +0100

    Update Program.cs

--- Soqet/Program.cs
+++ Soqet/Program.cs
@@ -2,56 +2,52 @@ using Microsoft.OpenApi.Models;
 using Soqet.Models;
 using System.Text.Json;
 
-namespace Soqet
+namespace Soqet;
+public class Program
 {
-    public class Program
+    private static readonly HashSet<Client> clients = new();
+
+    public static void Main(string[] args)
     {
+        var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddSingleton(clients);
+        builder.Services.AddSingleton<ServiceLogic>();
 
-        private static readonly HashSet<Client> clients = new();
-
-        public static void Main(string[] args)
+        builder.Services.AddControllers();
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(o =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddSingleton(clients);
-            builder.Services.AddSingleton<ServiceLogic>();
-
-            builder.Services.AddControllers();
-            // Add services to the container.
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(o =>
+            o.SwaggerDoc("v3", new()
             {
-                o.SwaggerDoc("v3", new()
+                Title = "Soqet",
+                Version = "v3",
+                Contact = new OpenApiContact
                 {
-                    Title = "Soqet",
-                    Version = "v3",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "AlexDevs",
-                        Email = "me@alexdevs.me",
-                        Url = new Uri("https://alexdevs.me"),
-                    },
-                });
-
-                o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Soqet.xml"));
+                    Name = "AlexDevs",
+                    Email = "me@alexdevs.me",
+                    Url = new Uri("https://alexdevs.me"),
+                },
             });
 
-            var app = builder.Build();
+            o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Soqet.xml"));
+        });
 
-            app.UseSwagger(c => { c.RouteTemplate = "/{documentname}/swagger.json"; });
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/v3/swagger.json", "Soqet V3");
-                c.DocumentTitle = "Soqet API Documentation";
-                c.RoutePrefix = "";
-            });
+        var app = builder.Build();
 
-            app.UseWebSockets();
-            app.MapControllers();
+        app.UseSwagger(c => { c.RouteTemplate = "/{documentname}/swagger.json"; });
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/v3/swagger.json", "Soqet V3");
+            c.DocumentTitle = "Soqet API Documentation";
+            c.RoutePrefix = "";
+        });
 
-            app.Run();
-        }
+        app.UseWebSockets();
+        app.MapControllers();
+
+        app.Run();
     }
 }
