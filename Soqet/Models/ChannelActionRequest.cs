5eaf3f3487438052bd42ff5367a889f82f87e7b7
Alessandro Proto <me@alexdevs.me>
2022-02-01 10:08:24 +0100

    Added support to open/close multiple channels at a time

--- Soqet/Models/ChannelActionRequest.cs
+++ Soqet/Models/ChannelActionRequest.cs
@@ -3,7 +3,8 @@
 #pragma warning disable CS8618
     public class ChannelActionRequest
     {
-        public string Channel { get; set; }
+        public string? Channel { get; set; }
+        public string[]? Channels { get; set; }
         public string Action { get; set; }
     }
 #pragma warning restore CS8618
